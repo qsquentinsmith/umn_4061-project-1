42c85dd3213a94466b9d7eff3d4b623be9508391
Harvey Duong <duong144@umn.edu>
2020-02-22 11:56:52 -0600

    Implement cmd_update_state()

--- cmd.c
+++ cmd.c
@@ -40,14 +40,42 @@ void cmd_start(cmd_t *cmd) {
     if (pid == 0) {
         dup2(cmd->out_pipe[PWRITE], STDOUT_FILENO);
         close(cmd->out_pipe[PREAD]);  // I feel like this is not right
-        execvp(cmd->argv[0], cmd->argv);
+        execvp(cmd->name, cmd->argv);
     } else {
         close(cmd->out_pipe[PWRITE]);  // I feel like this is not right
         cmd->pid = pid;  // Set child pid in the parent
     }
 }
 
-void cmd_update_state(cmd_t *cmd, int block);
+void cmd_update_state(cmd_t *cmd, int block) {
+    // If cmd is already finished, no need to update!
+    if (cmd->finished == 1) {
+        return;
+    }
+
+    // Try to wait for the process based on the block method
+    int status = waitpid(cmd->pid, &cmd->status, block);
+
+    // Interpret the waitpid return value, if it is 0, there are no status
+    // updates. If the value is -1, an error occurred and commando should
+    // exit. Otherwise, a state change has occurred and we update cmd to
+    // indicate that it has finished.
+    if (status == 0) {
+        return;
+    } else if (status == -1) {
+        printf("Error waiting for process %d to finish.\n", cmd->pid);
+        exit(0);
+    }
+    else {
+        if (WIFEXITED(cmd->status)) {
+            cmd->status = WEXITSTATUS(cmd->status);
+            snprintf(cmd->str_status, STATUS_LEN+1, "EXIT(%d)", cmd->status);
+            cmd->finished = 1;
+            cmd_fetch_output(cmd);
+            printf("@!!! %s[#%d]: %s\n", cmd->name, cmd->pid, cmd->str_status);
+        }
+    }
+}
 char *read_all(int fd, int *nread);
 void cmd_fetch_output(cmd_t *cmd);
 void cmd_print_output(cmd_t *cmd);
