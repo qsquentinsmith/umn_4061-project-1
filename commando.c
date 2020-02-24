8a48ba8da5a562f793c77c56a1535cc36f8a7c80
Harvey Duong <duong144@umn.edu>
2020-02-23 19:07:38 -0600

    Correct output

--- commando.c
+++ commando.c
@@ -61,11 +61,21 @@ int main(int argc, char **argv) {
             if ( jobnum >= cmds.size || jobnum < 0 ) {
                 continue;
             }
+            printf("@<<< Output for %s[#%d] (%d bytes):\n",
+                    cmds.cmd[jobnum]->name, cmds.cmd[jobnum]->pid,
+                    cmds.cmd[jobnum]->output_size);
+            printf("----------------------------------------\n");
             cmd_print_output(cmds.cmd[jobnum]);
+            printf("----------------------------------------\n");
         }
         else if ( strncmp(args[0], "output-all", NAME_MAX) == 0 ) {
             for (int i = 0; i < cmds.size; i++) {
+                printf("@<<< Output for %s[#%d] (%d bytes):\n",
+                        cmds.cmd[i]->name, cmds.cmd[i]->pid,
+                        cmds.cmd[i]->output_size);
+                printf("----------------------------------------\n");
                 cmd_print_output(cmds.cmd[i]);
+                printf("----------------------------------------\n");
             }
         }
         else if ( strncmp(args[0], "wait-for", NAME_MAX) == 0 ) {
